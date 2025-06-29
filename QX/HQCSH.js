7bfff3a5036c7f32311c42f48fc16592a8a1a4e5
明亮的夜空 <102882768+reverie1988@users.noreply.github.com>
2025-06-29 21:09:02 +0800

    Update HQCSH.js

--- QX/HQCSH.js
+++ QX/HQCSH.js
@@ -1,20 +1,14 @@
-const $ = new API("accountId-extractor");
-const accountId = $request.headers['accountid'];
+// 带持久化存储的版本
+const accountId = $request.headers['AccountId'] || $request.headers['accountId'];
+const savedId = $persistentStore.read("lastAccountId");
 
 if (accountId) {
-    $.notify("AccountID 提取成功", "", `提取到的 AccountID: ${accountId}`);
-    $.done({});
+    if (accountId !== savedId) {
+        $persistentStore.write(accountId, "lastAccountId");
+        $notify("发现新 AccountID", "", accountId);
+    }
+    $done({});
 } else {
-    $.notify("AccountID 提取失败", "", "未找到 AccountID 字段");
-    $.done({});
-}
-
-function API(name) {
-    this.name = name;
-    this.notify = (title, subtitle, body) => {
-        $notification.post(title, subtitle, body);
-    };
-    this.done = (obj) => {
-        $done(obj);
-    };
+    $notify("AccountID 提取失败", "", "检查请求头");
+    $done({});
 }
